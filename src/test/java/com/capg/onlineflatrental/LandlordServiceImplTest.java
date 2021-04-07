0df48d8a8bc7f697d5364a0b204c52e212d3a842
Nithisha2000 <nithisha7200@gmail.com>
2021-04-07 20:11:22 +0530

    Landlord testing

--- src/test/java/com/capg/onlineflatrental/LandlordServiceImplTest.java
+++ src/test/java/com/capg/onlineflatrental/LandlordServiceImplTest.java
@@ -1,13 +1,15 @@
 package com.capg.onlineflatrental;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.capg.onlineflatrental.entities.Flat;
 import com.capg.onlineflatrental.entities.FlatAddress;
 import com.capg.onlineflatrental.entities.Landlord;
@@ -19,135 +21,317 @@ class LandlordServiceImplTest {
 
 	@Autowired
 	ILandlordService service;
-	
+
 	Landlord landlord = null;
-	List<Flat> flatList =new ArrayList<Flat>();
-	FlatAddress flatAddress=null;
-	
+	List<Flat> flatList = new ArrayList<Flat>();
+	FlatAddress flatAddress = null;
+
 	@BeforeAll
 	public static void init() {
-		
+
 	}
 
 	@Test
 	void testAddLandlord01() throws LandlordNotFoundException {
-//		flatAddress=new FlatAddress(1,"street","city","state",000013,"country");
-//		
-//	    flatList(1,1000f,flatAddress,"Y");
-//		landlord = new Landlord(1, "Name", 24,flatList);
-//		assertNotNull(service.addLandlord(landlord));
-//	}
-//
-//	@Test
-//	void testAddLandlord02() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(2, null, 25, );
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Landlord name cannot be empty", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord03() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(3, "ni", 26,);
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord04() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(4, "", 40, );
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord05() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(5, "Name123", 24,);
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord06() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(6, "Name#$", 25, );
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
-//		}
-//	}
-//	
-//	
-//	@Test
-//	void testAddLandlord07() throws LandlordNotFoundException {
-//		landlord = new Landlord(7, "Name", 17, );
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Minor Age is not allowed", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord08() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(8, "Name", 0, );
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Age cannot be 0 or negative", exception.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	void testAddLandlord09() throws LandlordNotFoundException {
-//		
-//		landlord = new Landlord(9, "Name", -1,);
-//		try
-//		{
-//			service.addLandlord(landlord);
-//		}
-//		catch(LandlordNotFoundException exception)
-//		{
-//			assertEquals("Age cannot be 0 or negative", exception.getMessage());
-//		}
-//	}
-//	
-}}
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(1, "Name", 24, flatList);
+		assertNotNull(service.addLandlord(landlord));
+	}
+
+	@Test
+	void testAddLandlord02() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(2, null, 25, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord name cannot be empty", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord03() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(3, "ni", 26, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord04() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(4, "namenamenamenamenamenamenamenamenamename", 40, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord05() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(5, "Name123", 24, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord06() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(6, "Name#$", 25, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord07() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(7, "Name", 17, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Minor Age is not allowed", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord08() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(8, "Name", 0, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Age cannot be 0 or negative", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testAddLandlord09() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(9, "Name", -1, flatList);
+		try {
+			service.addLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Age cannot be 0 or negative", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord10() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(1, "Name", 24, flatList);
+		assertNotNull(service.updateLandlord(landlord));
+	}
+
+	@Test
+	void testUpdateLandlord11() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(11, "Name", 24, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("No Landlord found in given ID", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord12() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(12, null, 25, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord name cannot be empty", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord13() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(13, "na", 26, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+		}
+	}
+	
+
+	@Test
+	void testUpdateLandlord14() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(14, "namenamenamenamenamenamenamenamename", 40, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord15() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(15, "Name123", 24, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord16() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(16, "Name#$", 25, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord17() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		landlord = new Landlord(17, "Name", 17, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Minor Age is not allowed", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord18() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(18, "Name", 0, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Age cannot be 0 or negative", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testUpdateLandlord19() throws LandlordNotFoundException {
+		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+
+		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+
+		landlord = new Landlord(19, "Name", -1, flatList);
+		try {
+			service.updateLandlord(landlord);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("Age cannot be 0 or negative", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testDeleteTenant20() throws LandlordNotFoundException {
+		assertEquals(service.viewLandlord(219).getLandlordName(), service.deleteLandlord(219).getLandlordName());
+	}
+
+	@Test
+	void testDeleteLandlord21() throws LandlordNotFoundException {
+
+		try {
+			service.deleteLandlord(2);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("No Landlord found in the given Id", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testDeleteLandlord22() throws LandlordNotFoundException {
+
+		try {
+			service.deleteLandlord(0);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("No Landlord found in the given Id", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testViewLandlord23() throws LandlordNotFoundException {
+		assertEquals("Name", service.viewLandlord(50).getLandlordName());
+	}
+
+	@Test
+	void testViewUser24() throws LandlordNotFoundException {
+		try {
+			service.viewLandlord(1);
+		} catch (LandlordNotFoundException exception) {
+			assertEquals("No landlord found with that Id\r\n" + "\r\n" + "Enter valid UserId", exception.getMessage());
+		}
+	}
+
+	@Test
+	void testViewAllLandlord25() {
+		assertNotNull(service.viewAllLandlord());
+	}
+
+	@Test
+	void testViewAllLandlord26() throws LandlordNotFoundException {
+		try {
+			assertNull(service.viewAllLandlord());
+		} catch (AssertionFailedError exception) {
+			assertNotNull(service.viewAllLandlord());
+		}
+	}
+
+}
