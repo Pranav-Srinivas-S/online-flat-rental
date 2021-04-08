53a10f02142857a608aec895216071e0bcd337a8
Nithisha2000 <nithisha7200@gmail.com>
2021-04-08 12:08:39 +0530

    Landlord testing updated

--- src/test/java/com/capg/onlineflatrental/LandlordServiceImplTest.java
+++ src/test/java/com/capg/onlineflatrental/LandlordServiceImplTest.java
@@ -34,7 +34,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testAddLandlord01() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 		landlord = new Landlord(1, "Name", 24, flatList);
@@ -43,7 +43,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testAddLandlord02() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 		landlord = new Landlord(2, null, 25, flatList);
@@ -56,20 +56,20 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testAddLandlord03() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 		landlord = new Landlord(3, "ni", 26, flatList);
 		try {
 			service.addLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+			assertEquals("Landlord name length should be in range 3 to 30", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testAddLandlord04() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -77,13 +77,13 @@ class LandlordServiceImplTest {
 		try {
 			service.addLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+			assertEquals("Landlord name length should be in range 3 to 30", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testAddLandlord05() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -91,13 +91,13 @@ class LandlordServiceImplTest {
 		try {
 			service.addLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+			assertEquals("Landlord name cannot contain Numbers or Special Characters", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testAddLandlord06() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -105,13 +105,13 @@ class LandlordServiceImplTest {
 		try {
 			service.addLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+			assertEquals("Landlord name cannot contain Numbers or Special Characters", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testAddLandlord07() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 		landlord = new Landlord(7, "Name", 17, flatList);
@@ -124,7 +124,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testAddLandlord08() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -138,7 +138,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testAddLandlord09() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -152,19 +152,19 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testUpdateLandlord10() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
-		landlord = new Landlord(1, "Name", 24, flatList);
+		landlord = new Landlord(49, "Name", 24, flatList);
 		assertNotNull(service.updateLandlord(landlord));
 	}
 
 	@Test
 	void testUpdateLandlord11() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
-		landlord = new Landlord(11, "Name", 24, flatList);
+		landlord = new Landlord(6, "Name", 24, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
@@ -174,7 +174,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testUpdateLandlord12() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 		landlord = new Landlord(12, null, 25, flatList);
@@ -187,66 +187,65 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testUpdateLandlord13() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
-		landlord = new Landlord(13, "na", 26, flatList);
+		landlord = new Landlord(21, "na", 26, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+			assertEquals("Landlord name length should be in range 3 to 30", exception.getMessage());
 		}
 	}
-	
 
 	@Test
 	void testUpdateLandlord14() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
-		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
+		flatList.add(new Flat(24, 1000f, flatAddress, "Y"));
 
-		landlord = new Landlord(14, "namenamenamenamenamenamenamenamename", 40, flatList);
+		landlord = new Landlord(30, "namenamenamenamenamenamenamenamename", 40, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name length should be in range 3 to 30", exception.getMessage());
+			assertEquals("Landlord name length should be in range 3 to 30", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testUpdateLandlord15() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
-		landlord = new Landlord(15, "Name123", 24, flatList);
+		landlord = new Landlord(12, "Name123", 24, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+			assertEquals("Landlord name cannot contain Numbers or Special Characters", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testUpdateLandlord16() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
-		landlord = new Landlord(16, "Name#$", 25, flatList);
+		landlord = new Landlord(27, "Name#$", 25, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
-			assertEquals("Landlord Name cannot contain Numbers or Special Characters", exception.getMessage());
+			assertEquals("Landlord name cannot contain Numbers or Special Characters", exception.getMessage());
 		}
 	}
 
 	@Test
 	void testUpdateLandlord17() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
-		landlord = new Landlord(17, "Name", 17, flatList);
+		landlord = new Landlord(15, "Name", 17, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
@@ -256,7 +255,7 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testUpdateLandlord18() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
@@ -270,11 +269,11 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testUpdateLandlord19() throws LandlordNotFoundException, InvalidFlatInputException {
-		flatAddress = new FlatAddress(1, "street", "city", "state", 000013, "country");
+		flatAddress = new FlatAddress(1, "street", "city", "state", 600013, "country");
 
 		flatList.add(new Flat(1, 1000f, flatAddress, "Y"));
 
-		landlord = new Landlord(19, "Name", -1, flatList);
+		landlord = new Landlord(18, "Name", -1, flatList);
 		try {
 			service.updateLandlord(landlord);
 		} catch (LandlordNotFoundException exception) {
@@ -282,16 +281,16 @@ class LandlordServiceImplTest {
 		}
 	}
 
-	@Test
-	void testDeleteTenant20() throws LandlordNotFoundException {
-		assertEquals(service.viewLandlord(219).getLandlordName(), service.deleteLandlord(219).getLandlordName());
-	}
+//	@Test
+//	void testDeleteLandlord20() throws LandlordNotFoundException {
+//		assertEquals(service.viewLandlord(35).getLandlordName(), service.deleteLandlord(35).getLandlordName());
+//	}
 
 	@Test
 	void testDeleteLandlord21() throws LandlordNotFoundException {
 
 		try {
-			service.deleteLandlord(2);
+			service.deleteLandlord(109);
 		} catch (LandlordNotFoundException exception) {
 			assertEquals("No Landlord found in the given Id", exception.getMessage());
 		}
@@ -301,7 +300,7 @@ class LandlordServiceImplTest {
 	void testDeleteLandlord22() throws LandlordNotFoundException {
 
 		try {
-			service.deleteLandlord(0);
+			service.deleteLandlord(114);
 		} catch (LandlordNotFoundException exception) {
 			assertEquals("No Landlord found in the given Id", exception.getMessage());
 		}
@@ -309,13 +308,13 @@ class LandlordServiceImplTest {
 
 	@Test
 	void testViewLandlord23() throws LandlordNotFoundException {
-		assertEquals("Name", service.viewLandlord(50).getLandlordName());
+		assertEquals("Name", service.viewLandlord(12).getLandlordName());
 	}
 
 	@Test
-	void testViewUser24() throws LandlordNotFoundException {
+	void testViewLandlord24() throws LandlordNotFoundException {
 		try {
-			service.viewLandlord(1);
+			service.viewLandlord(12);
 		} catch (LandlordNotFoundException exception) {
 			assertEquals("No landlord found with that Id\r\n" + "\r\n" + "Enter valid UserId", exception.getMessage());
 		}
@@ -326,13 +325,6 @@ class LandlordServiceImplTest {
 		assertNotNull(service.viewAllLandlord());
 	}
 
-	@Test
-	void testViewAllLandlord26() throws LandlordNotFoundException {
-		try {
-			assertNull(service.viewAllLandlord());
-		} catch (AssertionFailedError exception) {
-			assertNotNull(service.viewAllLandlord());
-		}
-	}
-
+	
+	
 }
